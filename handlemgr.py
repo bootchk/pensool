690578bc918609217e81f8da9a0112d0fddcc161
Lloyd Konneker <bootch@nc.rr.com>
2011-02-17 07:57:07 -0500

    handles

---
+++ handlemgr.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+'''
+Handle manager: ensure only one set of handles active.
+
+HandleSet's are not in the model.
+An active HandleSet is in the scheme and is drawn.
+Generally Handle's draw on top of the morphs they handle.
+However, it may be drawn invisibly !!!  
+E.G. points may be hidden by lines they handle.
+E.G. style of handles may be invisible.
+A HandleSet is drawn and picked in the transform of its parent morph.
+A HandleSet is not a child of parent morph !!!
+'''
+
+import viewport
+from decorators import *
+
+
+current_handle_set = None
+current_morph = None
+
+
+@dump_event
+def rouse(handle_set, morph, direction):
+  ''' Rouse handle set on (visually) morph. '''
+  global current_handle_set, current_morph
+  
+  if direction:
+    current_handle_set = handle_set
+    current_morph = morph
+    # FIXME put in scheme or append to morph and later remove
+  else:
+    current_handle_set = None
+    current_morph = None
+
+@dump_return
+def pick(point):
+  ''' Pick any handle of the current handle set. '''
+  if current_handle_set:
+    context = viewport.viewport.user_context()
+    context.set_matrix(current_morph.retained_transform)
+    return current_handle_set.pick(context, point)
+  else:
+    # print "!!!!!!!!!!!!!!!!!!!!!!!!!!No current handle set"
+    return None
+    
+def draw():
+  ''' Draw current handle set. '''
+  if current_handle_set:
+    context = viewport.viewport.user_context()
+    context.set_matrix(current_morph.retained_transform)
+    return current_handle_set.draw(context)
+  
+
+
